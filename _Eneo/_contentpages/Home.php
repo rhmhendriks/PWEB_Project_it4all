e9bd123b1b9577ce79fe122105356c7b091a1335
luc9willemse <lucwillemse@live.nl>
2021-01-22 10:01:52 +0100

    placed some divs

---
+++ _Eneo/_contentpages/Home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eneo Home</title>
+</head>
+<body>
+    <div class="textHome">
+    <h1> Welcome to the Eneo website </h1>
+    <p> announcements </p>
+    </div>
+
+    <?php
+        echo '<div id="filter">';
+        echo 'hier moeten de filters';
+        echo '</div>';
+
+        echo '<div id="graph">';
+        echo 'hier moet de graph of tabel';
+        echo '</div>'
+    ?>
+
+</body>
+</html>
--- _contentpages/Home.php
+++ _contentpages/Home.php
@@ -5,20 +5,26 @@
 </head>
 <body>
 
+	<div id="homeHeader">
 	<h1> Welkom op onze website IT4ALL </h1>
 	<br>
 	<br>
 	<p style="color:red;">Deze website is een schoolproject! Bestelde producten zullen dan ook NOOIT aankomen, u zult alleen een mail kregen met deze boodschap. Accounts kunnen wel aangemaakt worden op deze site, en deze worden uiteraard met de hoogst mogelijke beveiliging toegevoegd aan de database. Dit wil zeggen dat uw persoonsgegevens en wachtwoord versleuteld worden opgeslagen (let op! Vul desondanks NOOIT uw eigen bangegevens in maar gebruik een dummy zoals GB33BUKB20201555555555 (dit is een genegeerd IBAN voor testdoeleinden). </p>
 	<br>
 	<br>
+	</div>
 
+	<div id="homePictures">
 	<p><a href="../index.php?page=articlelist&ID=100"><img src="../_images/Home/laptop.jpg" alt="laptop" height="200"></a>
 		<a href="../index.php?page=articlelist&ID=104"><img src="../_images/Home/smartphone.jpg" alt="telefoon" height="200"></a>
 		<a href="../index.php?page=articlelist&ID=109"><img src="../_images/Home/tv.jpg" alt="tv" height="200"></a>
 		<a href="../index.php?page=articlelist&ID=106"><img src="../_images/Home/games.jpg" alt="games" height="200"></a>
 	</p>
+	</div>
 	<br><br>
+	<div id="homeFooter">
 	<h3>Koop nu uw nieuwe snufjes</h3>
+	</div>
 
 </body>
 </html>
