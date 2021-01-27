7911010a717312a70cc26eb44b88a60fde564b12
rienanp267 <r.poortvliet@st.hanze.nl>
2021-01-27 12:40:35 +0100

    styling the policies

--- _contentpages/policy/algemene_voorwaarden.php
+++ _contentpages/policy/algemene_voorwaarden.php
@@ -1,40 +1,27 @@
 <!DOCTYPE HTML>
 
 <html lang="en">
-  <head>
-	<title>Algemene voorwaarden</title>
-  </head>
-  <body>
-	<h1>Algemene voorwaarden</h1>
-	
-	<h2>1.Algemeen</h2>
-		
-		<p>De algemene voorwaarden zijn van toepassing op alle aanbiedingen, offertes en overeenkomsten voor bestelling, betaling en overdracht tussen de webwinkel en klanten.</p>
-		
-		<p>Alle gepubliceerde en decommuniceerde tarieven zijn tarieven exclusief 21% BTW.</p>
-
-		<p>Een account op onze site is verbonden aan één persoon.</p>
-		
-	<h2>2.Aansprakelijkheid</h2>
-	
-		<p>Schade die wordt veroorzaakt door gebrekken aan het systeem zal worden vergoed. Indien de klant zelf enige vorm van schade veroorzaakt aan zijn/haar gekochte product zal de schade niet worden vergoed.</p>
-		
-	<h2>3.Annulering door de webwinkel</h2>
-	
-		<p>	De webwinkel streeft ernaar de bestelde artikelen af te kunnen leveren. Als door onvoorzien omstandigheden een bestelling moet worden geannuleerd of verplaatst, geld met betrekking tot de aansprakelijkheid voor schade die kan ontstaan als gevolg van de annulering. Door de webwinkel, dat het betaald bedrag door de webwinkel wordt uitgekeerd.
-
-			Wanneer de klant een dienst wil annuleren zijn zij verplicht alle gemaakte kosten die op dat moment zijn gemaakt wel te vergoeden.</p>
-	
-	<h2>4.Vertrouwelijkheid</h2>
-	
-		<p>De webwinkel zal ten aanzien van het gebruik van de hem in de klantrelatie verstrekte of ter kennis gekomen vertrouwelijke informatie geheimhouding betrachten.</p>
-	
-	<h2>5.Auteursrecht</h2>
-	
-		<p>Alle materialen die worden aangekocht zijn auteursrechtelijk beschermd en mogen niet worden vermenigvuldigd zonder uitdrukkelijke toestemming van de auteur(s). Het is niet toegestaan materiaal geheel of gedeeltelijk aan derden af te staan.</p>
-	
-	<h2>6.Betalingsvoorwaarde</h2>
-
-		<p>Is de webwinkel in verzuim of schiet hij op een andere wijze tekort in het nakomen van één of meer verplichtingen? Dan komen alle redelijke kosten ter verkrijging van voldoening voor zijn rekening, zowel de gerechtelijke als de buitengerechtelijke.</p>
-  </body>
+  	<head>
+		<title>Algemene voorwaarden</title>
+  	</head>
+  	<body>
+		<div id="policies">
+			<h1>Algemene voorwaarden</h1>
+			<h2>1.Algemeen</h2>
+				<p>De algemene voorwaarden zijn van toepassing op alle aanbiedingen, offertes en overeenkomsten voor bestelling, betaling en overdracht tussen de webwinkel en klanten.</p>
+				<p>Alle gepubliceerde en decommuniceerde tarieven zijn tarieven exclusief 21% BTW.</p>
+				<p>Een account op onze site is verbonden aan één persoon.</p>
+			<h2>2.Aansprakelijkheid</h2>
+				<p>Schade die wordt veroorzaakt door gebrekken aan het systeem zal worden vergoed. Indien de klant zelf enige vorm van schade veroorzaakt aan zijn/haar gekochte product zal de schade niet worden vergoed.</p>
+			<h2>3.Annulering door de webwinkel</h2>
+				<p>	De webwinkel streeft ernaar de bestelde artikelen af te kunnen leveren. Als door onvoorzien omstandigheden een bestelling moet worden geannuleerd of verplaatst, geld met betrekking tot de aansprakelijkheid voor schade die kan ontstaan als gevolg van de annulering. Door de webwinkel, dat het betaald bedrag door de webwinkel wordt uitgekeerd.
+					Wanneer de klant een dienst wil annuleren zijn zij verplicht alle gemaakte kosten die op dat moment zijn gemaakt wel te vergoeden.</p>
+			<h2>4.Vertrouwelijkheid</h2>
+				<p>De webwinkel zal ten aanzien van het gebruik van de hem in de klantrelatie verstrekte of ter kennis gekomen vertrouwelijke informatie geheimhouding betrachten.</p>
+			<h2>5.Auteursrecht</h2>
+				<p>Alle materialen die worden aangekocht zijn auteursrechtelijk beschermd en mogen niet worden vermenigvuldigd zonder uitdrukkelijke toestemming van de auteur(s). Het is niet toegestaan materiaal geheel of gedeeltelijk aan derden af te staan.</p>
+			<h2>6.Betalingsvoorwaarde</h2>
+				<p>Is de webwinkel in verzuim of schiet hij op een andere wijze tekort in het nakomen van één of meer verplichtingen? Dan komen alle redelijke kosten ter verkrijging van voldoening voor zijn rekening, zowel de gerechtelijke als de buitengerechtelijke.</p>
+		</div>
+	</body>
 </html>
