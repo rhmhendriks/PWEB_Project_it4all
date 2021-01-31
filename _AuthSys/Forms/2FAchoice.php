17cf5fbb0c03bb904fa25264ae13526b4c45016f
rhmhendriks <rhm.hendriks@outlook.com>
2021-01-31 15:22:24 +0100

    2fachoice

---
+++ _AuthSys/Forms/2FAchoice.php
@@ -0,0 +1,18 @@
+<?php session_start() ?>
+    <div id="2FAchoice">
+        <form method="post" action="../_AuthSys/Controllers/2FAchoiceController.php" class="authenticationforms-container">
+        <h1> Tweede verificatiemethode </h1>
+                <?php if (isset($_SESSION['Message_Act']) || isset($_SESSION['Message_si'])){ echo '<p style="color:Red">' . $_SESSION['Message_Act'] . "<br>" . $_SESSION['Message_si'] . '<br></p>'; } if (DebugisOn && isset($_SESSION['Debug_Act']) || DebugisOn && isset($_SESSION['Debug_si']) ){ echo '<p style="color:Red">' . $_SESSION['Debug_Act'] . "<br" . $_SESSION['Debug_si'] . '</p>';}?>
+                <div id="inloggengegevens">
+                    <label>E-mail<span class="redStar">*</span></label>                  <input type="email" name="gebruikersnaam" id="gebruikersnaam" size="40"	pattern="[a-Z0-9._-]+@[a-Z0-9.-]+\.[a-Z]{2,}$"  placeholder="Voer je mailadres in" required>
+                    <label>Wachtwoord<span class="redStar">*</span></label>              <input type="password" name="wachtwoord" id="wachtwoord" size="40"> 
+                </div>
+                <div id="submitloginform">
+                    <input type="submit" class= "btn" value="inloggen" name="inloggen" id="inloggen">
+                </div>
+                <div id="linksloginform">
+                    <p><a href="index.php?page=auth&auth=ForgotPassword">Wachtwoord vergeten?</a></p>
+                    <p>Je kunt je hier <a href="index.php?page=auth&auth=SignUp">registreren</a></p>
+                </div>
+        </form>  
+    </div>
