5ffb3bede85b594fe31859588879b18b258a2600
luc9willemse <lucwillemse@live.nl>
2021-02-02 23:18:08 +0100

    Made table much faster

--- _Eneo/_Scripting/map/map.php
+++ _Eneo/_Scripting/map/map.php
@@ -20,7 +20,7 @@
 include $_SERVER['DOCUMENT_ROOT'] . "/_init/functionlibary.php";
 
 $json = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=649100-647000-646500-644000-644500-645000-645010');
-
+echo $json
 ?>
 <style>
 .mapboxgl-popup {
--- _Eneo/_contentpages/tables/table.php
+++ _Eneo/_contentpages/tables/table.php
@@ -14,19 +14,49 @@ table, th, td {
 <body>
 
 <?php 
-include $_SERVER['DOCUMENT_ROOT'] . "/_init/functionlibary.php";
+$json = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=649100-647000-646500-644000-644500-645000-645010-10010');
 
-$jsonCameroon = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=649100');
-$jsonChad = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=647000');
-$jsonCAR = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=646500');
-$jsonCongo = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=644000');
-$jsonCongo2 = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=644500');
-$jsonGabon = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=645000');
-$jsonGabon2 = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?token=JUR324HVJH2RGJH34J5J2VJHB43HJEJH23H42HGR3&from=02-01-2021&til=02-02-2021&filetype=JSON&type=T&stations=645010');
+function calculator($json) {
+    $array = json_decode($json, true);
+    $highest = -100;
+    $lowest = 100;
+    $total = 0;
+    $i = 0;
+    $list = array();
+    $countryCode = 0;
+    foreach($array as $key => $value) {
+        foreach($value as $key2 => $value2) {
+            if ($key2 == "stn") {
+                if ($value2 != $countryCode && $i != 0) {
+                    $countryCode = $value2;
+                    $average = $total / $i;
+                    array_push($list, array(round($highest, 1), round($lowest, 1), round($average, 1), $countryCode));
+                    $highest = -100;
+                    $lowest = 100;
+                    $average = 0;
+                    $total = 0;
+                    $i = 0;
+                } 
+            }
+            elseif ($key2 == "Temperatuur") {
+                $i += 1;
+                $total += $value2;
+                if ($value2 < $lowest) {
+                    $lowest = $value2;
+                }
+                elseif ($value2 > $highest) {
+                    $highest = $value2;
+                }
+            }
+        }
+    }
+    return $list;
+} 
 ?>
 
 <h2>Heat table</h2>
-<table class="generaltable">
+
+<table style="width:100%">
   <tr>
     <th>Weather station</th>
     <th>Highest temperature</th>
@@ -35,45 +65,45 @@ $jsonGabon2 = file_get_contents('https://it4all.rhmhendriks.nl/_API/index.php?to
   </tr>
   <tr>
     <td>Cameroon</td>
-    <td><?php echo calculator($jsonCameroon)[0]; ?></td>
-    <td><?php echo calculator($jsonCameroon)[1]; ?></td>
-    <td><?php echo calculator($jsonCameroon)[2]; ?></td>
+    <td><?php echo calculator($json)[1][0]; ?></td>
+    <td><?php echo calculator($json)[1][1]; ?></td>
+    <td><?php echo calculator($json)[1][2]; ?></td>
   </tr>
   <tr>
     <td>Chad</td>
-    <td><?php echo calculator($jsonChad)[0]; ?></td>
-    <td><?php echo calculator($jsonChad)[1]; ?></td>
-    <td><?php echo calculator($jsonChad)[2]; ?></td>
+    <td><?php echo calculator($json)[2][0]; ?></td>
+    <td><?php echo calculator($json)[2][1]; ?></td>
+    <td><?php echo calculator($json)[2][2]; ?></td>
   </tr>
   <tr>
     <td>Central African Republic</td>
-    <td><?php echo calculator($jsonCAR)[0]; ?></td>
-    <td><?php echo calculator($jsonCAR)[1]; ?></td>
-    <td><?php echo calculator($jsonCAR)[2]; ?></td>
+    <td><?php echo calculator($json)[3][0]; ?></td>
+    <td><?php echo calculator($json)[3][1]; ?></td>
+    <td><?php echo calculator($json)[3][2]; ?></td>
   </tr>
   <tr>
     <td>Congo</td>
-    <td><?php echo calculator($jsonCongo)[0]; ?></td>
-    <td><?php echo calculator($jsonCongo)[1]; ?></td>
-    <td><?php echo calculator($jsonCongo)[2]; ?></td>
+    <td><?php echo calculator($json)[4][0]; ?></td>
+    <td><?php echo calculator($json)[4][1]; ?></td>
+    <td><?php echo calculator($json)[4][2]; ?></td>
   </tr>
   <tr>
     <td>Congo</td>
-    <td><?php echo calculator($jsonCongo2)[0]; ?></td>
-    <td><?php echo calculator($jsonCongo2)[1]; ?></td>
-    <td><?php echo calculator($jsonCongo2)[2]; ?></td>
+    <td><?php echo calculator($json)[5][0]; ?></td>
+    <td><?php echo calculator($json)[5][1]; ?></td>
+    <td><?php echo calculator($json)[5][2]; ?></td>
   </tr>
   <tr>
     <td>Gabon</td>
-    <td><?php echo calculator($jsonGabon)[0]; ?></td>
-    <td><?php echo calculator($jsonGabon)[1]; ?></td>
-    <td><?php echo calculator($jsonGabon)[2]; ?></td>
+    <td><?php echo calculator($json)[6][0]; ?></td>
+    <td><?php echo calculator($json)[6][1]; ?></td>
+    <td><?php echo calculator($json)[6][2]; ?></td>
   </tr>
   <tr>
     <td>Gabon</td>
-    <td><?php echo calculator($jsonGabon2)[0]; ?></td>
-    <td><?php echo calculator($jsonGabon2)[1]; ?></td>
-    <td><?php echo calculator($jsonGabon2)[2]; ?></td>
+    <td><?php echo calculator($json)[7][0]; ?></td>
+    <td><?php echo calculator($json)[7][1]; ?></td>
+    <td><?php echo calculator($json)[7][2]; ?></td>
   </tr>
 </table>
 
