3153eabe60b582fe4d423102886c48d086f3997d
rhmhendriks <rhm.hendriks@outlook.com>
2021-02-03 11:21:02 +0100

    Map

--- _Eneo/_contentpages/Home.php
+++ _Eneo/_contentpages/Home.php
@@ -4,10 +4,12 @@
 </div>
 
 <?php
+require "_Scripting/map/mapHead.php";
 echo '<div id="homemap">';
-require "../_Scripting/map/map.php";
+echo '<div id="map"></div>';
 echo '</div>';
 
 echo '<div id="homegraph">';
 echo '</div>';
+require "_Scripting/map/script.php";
 ?>
