029535466305edd0770731936891c8d7611e7b85
rhmhendriks <rhm.hendriks@outlook.com>
2021-02-03 13:54:48 +0100

    map

---
+++ _Eneo/_Scripting/map/mapFrame.php
@@ -0,0 +1,1 @@
+<iframe src="https://it4all.rhmhendriks.nl/_Eneo/_Scripting/map/map.php" frameborder="0" cellspacing="0" style="border-style: none; width: 95%; height: 95%; margin: auto;"></iframe> 
