7582415ae807bef7c69dc012a5ec897b4ba36ecd
rienanp267 <r.poortvliet@st.hanze.nl>
2021-02-03 14:00:27 +0100

    heatmap

--- _Eneo/_Scripting/menu/menu.php
+++ _Eneo/_Scripting/menu/menu.php
@@ -36,13 +36,12 @@
                     <li class="liFade"><a href="#">Temperature</a>
                         <ul>
                             <li><a href="index.php?heatmap=y">Heatmap</a></li>
-                            <li><a href="index.php?">Table</a></li>
+                            <li><a href="index.php?">Graph</a></li>
                         </ul> 
                     </li>
                     <li class="liFade"><a href="#">Wind speed</a>
                         <ul>
                             <li><a href="index.php?">Graph</a></li>
-                            <li><a href="index.php?">Table</a></li>
                         </ul> 
                     </li>
                     <li class="liFade"><a href="">Download data</a>
